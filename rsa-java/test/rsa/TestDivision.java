4be2d2d8cc68260e4c48c8d599f1b71e96015235
Peter Magnusson <psjm@Peter-Magnussons-MacBook-Pro.local>
2015-03-04 10:48:18 +0100

    added basic trial division implementation of modulus

--- rsa-java/test/rsa/TestDivision.java
+++ rsa-java/test/rsa/TestDivision.java
@@ -21,16 +21,37 @@ public class TestDivision {
 		}
 		return P >> n;
 	}
-
-	// -- Where: N = Numerator, D = Denominator, n = #bits, P = Partial
-	// remainder, q(i) = bit #i of quotient
-
+	
 	@Test
 	public void test() {
-		for (int n = 2; n < 13; n++)
+		for (int n = 2; n < 5; n++)
 			for (int D = 2; D <= (1 << n) - 1; D++)
 				for (int N = 1; N <= (1 << n) - 1; N++) {
 					int reminder = mod(N, D, n);
+					int modulus = N % D;
+					String msg = String
+							.format("(div( %d, %d, %d) yelds reminder: %d (expected: %d)\n",
+									N, D, n, reminder, modulus);
+					assertEquals(msg, reminder, modulus);
+				}
+	}
+
+	int mod2(int N, int D, int n) {
+		int P = N;
+		int T = N;
+		while(P>=0) {
+			T = P;
+			P -= D;
+		}
+		return T;
+	}
+
+	@Test
+	public void test2() {
+		for (int n = 2; n < 4; n++)
+			for (int D = 2; D <= (1 << n) - 1; D++)
+				for (int N = 1; N <= (1 << n) - 1; N++) {
+					int reminder = mod2(N, D, n);
 					int modulus = N % D;
 					String msg = String
 							.format("(div( %d, %d, %d) yelds reminder: %d (expected: %d)\n",

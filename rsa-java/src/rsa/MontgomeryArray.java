c1c5cb63030cc5971e83759952a3d92c87fc81ca
Peter Magnusson <psjm@Peter-Magnussons-MacBook-Pro.local>
2015-03-09 14:05:01 +0100

    remove unused code and comment out debug prints

--- rsa-java/src/rsa/MontgomeryArray.java
+++ rsa-java/src/rsa/MontgomeryArray.java
@@ -45,15 +45,6 @@ public class MontgomeryArray {
 	}
 
 	static void modulus_array(int length, int[] a, int[] modulus, int[] reminder) {
-
-		/*
-		 * int temp[] = new int[length]; copy_array(length, a, temp); //long P =
-		 * N; copy_array(length, a, reminder); //long T = N; while ((temp[0] &
-		 * 0x80000000) == 0) { //while(P>=0) { //debugArray("T= ", 3, temp);
-		 * copy_array(length, temp, reminder); //T = P; sub_array(length, temp,
-		 * modulus, temp); //P -= D; }
-		 */
-
 		int[] tmp = new int[length];
 		int[] tmp2 = new int[length];
 		copy_array(length, a, reminder);
@@ -79,49 +70,31 @@ public class MontgomeryArray {
 		for (int i = 0; i < 2 * N ; i++) {
 			shift_left_1_array(length, Nr, Nr);
 			modulus_array(length, Nr, M, Nr);
-//			debugArray(length, Nr);
 		}
-		// Nr = (2 ** 2N) mod M
-	}
-
-	private static int findN(int length, int[] E) {
-		for (int wordcnt = 0; wordcnt < length; wordcnt++) {
-			int e = E[wordcnt];
-			if (e == 0)
-				continue;
-			for (int bitcnt = 31; bitcnt >= 0; bitcnt--) {
-				if (((e >>> bitcnt) & 1) == 1) {
-					return 1 + bitcnt + 32 * (length - wordcnt - 1);
-				}
-			}
-		}
-		return 0;
 	}
 
 	public static void mont_exp_array(int length, int[] X, int[] E, int[] M,
 			int[] Nr, int[] P, int[] ONE, int[] temp, int[] Z) {
-		debugArray("X ", length, X);
-		debugArray("E ", length, E);
-		debugArray("M ", length, M);
+		//debugArray("X ", length, X);
+		//debugArray("E ", length, E);
+		//debugArray("M ", length, M);
 
-		//int n = findN(length, E);
-		//int n = 32;
 		int n = 32 * length;
-		System.out.println("N: " + n);
+		//System.out.println("N: " + n);
 
 		// 1. Nr := 2 ** 2N mod M
 		m_residue_2_2N_array(length, n, M, temp, Nr);
-		debugArray("Nr", length, Nr);
+		//debugArray("Nr", length, Nr);
 
 		// 2. Z0 := MontProd( 1, Nr, M )
 		zero_array(length, ONE);
 		ONE[length - 1] = 1;
 		mont_prod_array(length, ONE, Nr, M, Z);
-		debugArray("Z0", length, Z);
+		//debugArray("Z0", length, Z);
 
 		// 3. P0 := MontProd( X, Nr, M );
 		mont_prod_array(length, X, Nr, M, P);
-		debugArray("P0", length, P);
+		//debugArray("P0", length, P);
 
 		// 4. for i = 0 to n-1 loop
 		for (int i = 0; i < n; i++) {
@@ -131,18 +104,18 @@ public class MontgomeryArray {
 			if (ei == 1) {
 				mont_prod_array(length, Z, P, M, temp);
 				copy_array(length, temp, Z);
-				debugArray("Z ", length, Z);
+				//debugArray("Z ", length, Z);
 			}
 			// 5. Pi+1 := MontProd( Pi, Pi, M );
 			mont_prod_array(length, P, P, M, temp);
 			copy_array(length, temp, P);
-			debugArray("P ", length, P);
+			//debugArray("P ", length, P);
 
 		} // 7. end for
 			// 8. Zn := MontProd( 1, Zn, M );
 		mont_prod_array(length, ONE, Z, M, temp);
 		copy_array(length, temp, Z);
-		debugArray("Z ", length, Z);
+		//debugArray("Z ", length, Z);
 		// 9. RETURN Zn
 	}
 

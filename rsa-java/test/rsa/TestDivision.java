c3c27f7c773eace7b44635bd4048671278a3c9a8
Peter Magnusson <psjm@Peter-Magnussons-MacBook-Pro.local>
2015-03-09 13:55:24 +0100

    test implementations for modulus

--- rsa-java/test/rsa/TestDivision.java
+++ rsa-java/test/rsa/TestDivision.java
@@ -5,12 +5,15 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestDivision {
-	int mod(int N, int D, int n) {
-		int P = N;
+	private static final long ITERATION_LEVEL = 5;
+	
+	//Additional cost n*2 + n*2 + N*2 (P, T, doubled N) 
+	long mod(long N, long D, long n) {
+		long P = N;
 		D = D << n;// -- P and D need twice the word width of N and Q
 		// for i = n-1..0 do -- for example 31..0 for 32 bits
-		for (int i = n - 1; i >= 0; i--) {
-			int T = P << 1;
+		for (long i = n - 1; i >= 0; i--) {
+			long T = P << 1;
 			P = T - D;// -- trial subtraction from shifted value
 			if (P >= 0) {
 				//do not care about setting Q
@@ -24,11 +27,11 @@ public class TestDivision {
 	
 	@Test
 	public void test() {
-		for (int n = 2; n < 5; n++)
-			for (int D = 2; D <= (1 << n) - 1; D++)
-				for (int N = 1; N <= (1 << n) - 1; N++) {
-					int reminder = mod(N, D, n);
-					int modulus = N % D;
+		for (long n = 2; n < ITERATION_LEVEL; n++)
+			for (long D = 2; D <= (1 << n) - 1; D++)
+				for (long N = 1; N <= (1 << n) - 1; N++) {
+					long reminder = mod(N, D, n);
+					long modulus = N % D;
 					String msg = String
 							.format("(div( %d, %d, %d) yelds reminder: %d (expected: %d)\n",
 									N, D, n, reminder, modulus);
@@ -36,9 +39,10 @@ public class TestDivision {
 				}
 	}
 
-	int mod2(int N, int D, int n) {
-		int P = N;
-		int T = N;
+	// additional cost: n*2 (P,T). Very slow.
+	long mod2(long N, long D, long n) {
+		long P = N;
+		long T = N;
 		while(P>=0) {
 			T = P;
 			P -= D;
@@ -48,16 +52,48 @@ public class TestDivision {
 
 	@Test
 	public void test2() {
-		for (int n = 2; n < 4; n++)
-			for (int D = 2; D <= (1 << n) - 1; D++)
-				for (int N = 1; N <= (1 << n) - 1; N++) {
-					int reminder = mod2(N, D, n);
-					int modulus = N % D;
+		for (long n = 2; n < ITERATION_LEVEL; n++)
+			for (long D = 2; D <= (1 << n) - 1; D++)
+				for (long N = 1; N <= (1 << n) - 1; N++) {
+					long reminder = mod2(N, D, n);
+					long modulus = N % D;
 					String msg = String
 							.format("(div( %d, %d, %d) yelds reminder: %d (expected: %d)\n",
 									N, D, n, reminder, modulus);
 					assertEquals(msg, reminder, modulus);
 				}
 	}
+
+	long mod3(long N, long D, long n) {
+		long P = N;
+		long T;
+		for (long i = n - 1; i >= 0; i--) {
+			T = P;
+			P = T - (D << i);// -- trial subtraction from shifted value
+//			System.out.prlongf("i=%d N=%d D=%d T=%d P=%d\n", i, N, D, T, P);
+			if (P >= 0) {
+				//do not care about setting Q
+			} else {
+				//do not care about setting Q
+				P = T;
+			}
+		}
+		return P;
+	}
+
+	@Test
+	public void test3() {
+		for (long n = 2; n < ITERATION_LEVEL; n++)
+			for (long D = 2; D <= (1 << n) - 1; D++)
+				for (long N = 1; N <= (1 << n) - 1; N++) {
+					long reminder = mod3(N, D, n);
+					long modulus = N % D;
+					String msg = String
+							.format("(div( %d, %d, %d) yelds reminder: %d (expected: %d)\n",
+									N, D, n, reminder, modulus);
+					assertEquals(msg, modulus, reminder);
+				}
+	}
+
 
 }
